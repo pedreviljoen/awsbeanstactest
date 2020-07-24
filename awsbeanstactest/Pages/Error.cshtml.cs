a0eeed1c99ba8b2678a6ae2a3f6bf035413ec4ee
malak3ysa12 <53011237+malak3ysa12@users.noreply.github.com>
2020-07-24 23:16:25 +0200

    Add files via upload

--- awsbeanstactest/Pages/Error.cshtml.cs
+++ awsbeanstactest/Pages/Error.cshtml.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace awsbeanstactest.Pages
+namespace awscorewebapplication.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
@@ -14,6 +15,13 @@ namespace awsbeanstactest.Pages
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
 
         public void OnGet()
         {
